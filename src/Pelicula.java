f327e546dcc07670efbc6e09e3c9368de75a0b76
Sebastian Tobito <sebastiantobito@gmail.com>
2024-04-12 10:53:08 -0500

    subiendo clase pelicula

---
+++ src/Pelicula.java
@@ -0,0 +1,11 @@
+public class Pelicula {
+    public static void main(String[] args) {
+
+        String nombre;
+        int fechaDeLanzamiento;
+        int duracionEnMinutos;
+        boolean incluidaEnElPlan;
+
+
+    }
+}
