7af2ea94ebe4c55a297db92e50ecd676ee57f2cd
Sebastian Tobito <sebastiantobito@gmail.com>
2024-04-12 19:29:43 -0500

    subiendo clase principal, agregando metodos y películas

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,11 +1,27 @@
 public class Pelicula {
-    public static void main(String[] args) {
 
         String nombre;
         int fechaDeLanzamiento;
         int duracionEnMinutos;
         boolean incluidaEnElPlan;
+        double sumaDePuntajes;
+        int totalDeEvaluaciones;
+
+        void muestraFichaTecnica(){
+            System.out.println("\nEl nombre de la pelicula es: " + nombre);
+            System.out.println("El año del lanzamiento es: " + fechaDeLanzamiento);
+            System.out.println("Su duracion es de: " + duracionEnMinutos);
+        }
+
+        void evalua(double nota){
+            sumaDePuntajes = sumaDePuntajes + nota;
+            totalDeEvaluaciones++;
+        }
+
+        double calculaMedia(){
+            return sumaDePuntajes / totalDeEvaluaciones;
+
+        }
 
 
-    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,24 @@
 public class Principal {
     public static void main(String[] args) {
 
+    Pelicula miPelicula = new Pelicula();
+    miPelicula.nombre = "Encanto";
+    miPelicula.fechaDeLanzamiento = 2021;
+    miPelicula.duracionEnMinutos = 120;
+
+    miPelicula.muestraFichaTecnica();
+    miPelicula.evalua(8.1);
+    miPelicula.evalua(7.9);
+        System.out.println(miPelicula.sumaDePuntajes);
+        System.out.println(miPelicula.totalDeEvaluaciones);
+        System.out.println(miPelicula.calculaMedia());
+
+    Pelicula otraPelicula = new Pelicula();
+    otraPelicula.nombre = "Soy leyenda";
+    otraPelicula.fechaDeLanzamiento = 2006;
+    otraPelicula.duracionEnMinutos = 110;
+
+//        otraPelicula.muestraFichaTecnica();
     }
+
 }
